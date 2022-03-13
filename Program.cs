8536fd44612b544f49d0fd7c7a63dbfd8b0418d3
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 13:30:53 +0530

    Added LeapYear Program File

--- Program.cs
+++ Program.cs
@@ -4,34 +4,25 @@ namespace Day5FunctionalProblem
 {
     internal class Program
     {
-        public static void FlipCoin()
+        public static void LeapYear()
         {
-            Console.WriteLine("Hello, Welcome to Coin Flip Program");
-            int heads = 0;
-            int tails = 0;
 
-            Console.WriteLine("Enter the Coin flipping operation range: ");
-            int count = int.Parse(Console.ReadLine());
-            while (count > 0)
+            Console.WriteLine("Hello, Welcome to Leap Year Problem ");
+            Console.WriteLine("Enter year to check: ");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
             {
-                Random rand = new Random();
-                int randomCheck = rand.Next(2);
-                if (randomCheck == 0)
-                {
-                    heads++;
-                }
-                else
-                {
-                    tails++;
-                }
-                count--;
+                Console.WriteLine("This year is a Leap Year");
             }
-            Console.WriteLine("Number of heads is: " + heads);
-            Console.WriteLine("Number of Tails is: " + tails);
+            else
+            {
+                Console.WriteLine("Is not a Leap Year");
+            }
         }
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Program.FlipCoin();
+            Program.LeapYear();
         }
     }
 }
