4ade0e677a2bfb8330c70fb53ac3b9cfd9ba6a40
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 14:57:20 +0530

    Added program file of Largest Among 3v number Program

--- Program.cs
+++ Program.cs
@@ -4,37 +4,31 @@ namespace Day5FunctionalProblem
 {
     internal class Program
     {
-        public static void VowelOrConsonant()
+        public static void LargestAmong3()
         {
-            Console.WriteLine("Enter a Alphabet");
-            char ch = Convert.ToChar(Console.ReadLine().ToLower());
-            switch (ch)
+            Console.WriteLine("Welcome to largest among three number program: ");
+            Console.WriteLine("Please enter 3 number: ");
+            int first=int.Parse(Console.ReadLine());
+            int second=int.Parse(Console.ReadLine());
+            int third=int.Parse(Console.ReadLine());
+            if (first >= second && first >= third)
             {
-                case 'a':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'i':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'o':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'u':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'e':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                default:
-                    Console.WriteLine("The Alphabet is a Consonant");
-                    break;
+                Console.WriteLine("The largest number is: " + first);
+            }
+            else if (second >= third)
+            {
+                Console.WriteLine("The Largest Number is: "+second);
+            }
+            else
+            {
+                Console.WriteLine("The largest Number is: "+third);
             }
 
         }
 
         public static void Main(string[] args)
         {
-            Program.VowelOrConsonant();
+            Program.LargestAmong3();
         }
     }
 }
