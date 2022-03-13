fb537561d1179396174c546d6460314ea1a5f65b
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 13:52:43 +0530

    Added program file of Factorial

--- Program.cs
+++ Program.cs
@@ -4,31 +4,21 @@ namespace Day5FunctionalProblem
 {
     internal class Program
     {
-        public static void HramonicNumber()
+        public static void Factor()
         {
-            Console.WriteLine("Welcome to Harmonic Number Problem");
-            Console.WriteLine("Please Enter the Range: ");
-            int range = int.Parse(Console.ReadLine());
-            if (range == 0 || range < 0)
+            int fact=1;
+            Console.WriteLine("Enter a number to check: ");
+            int num=int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
             {
-                Console.WriteLine("Please Enter Non-Zero range");
+                fact = fact * i;
             }
-            else
-            {
-
-                float result = 0;
-                int n = range;
-                for (float i = 1; i <= range; i++)
-                {
-                    result = result + 1 / i;
-                }
-                Console.WriteLine("Harmonic Number upto the range" + n + "is: " + result);
-            }
+            Console.WriteLine("The factorial of "+num+" is: "+fact);
         }
 
         public static void Main(string[] args)
         {
-            Program.HramonicNumber();
+            Program.Factor();
         }
     }
 }
